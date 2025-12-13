09f0400a071aa7b9ec5ea0f7643c1f7e23adaa50
TDat <23130050@st.hcmuaf.edu.vn>
2025-12-13 22:45:47 +0700

    sua lai

--- web_app/main/views.py
+++ web_app/main/views.py
@@ -54,6 +54,30 @@ def get_changepw(request):
 def get_history(request):
     return render(request, 'main/history.html')
 
+def get_adminPage1(request):
+    return render(request, 'main/adminPage1.html')
+
+def get_adminPage2(request):
+    return render(request, 'main/adminPage2.html')
+
+def get_adminPage3(request):    
+    return render(request, 'main/adminPage3.html')
+
+def get_adminPage4(request):    
+    return render(request, 'main/adminPage4.html')
+
+def get_adminPage5(request):    
+    return render(request, 'main/adminPage5.html')
+
+def get_adminPage6(request):    
+    return render(request, 'main/adminPage6.html')
+
+def get_adminPage7(request):    
+    return render(request, 'main/adminPage7.html')
+
+def get_adminPage8(request):    
+    return render(request, 'main/adminPage8.html')
+
 def get_register(request):
     if request.method == 'POST':
         yourname = request.POST.get('yourname')
@@ -246,7 +270,6 @@ def get_cart(request):
         'total_price': total_price
     }
     return render(request, 'main/cart.html', context)
-
 
 
 
